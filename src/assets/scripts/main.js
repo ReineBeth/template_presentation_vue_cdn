eb3617daae9c963a30ea94bf6a2384d1ee700c2d
ReineBeth <simplement.eli@hotmail.com>
2021-11-23 16:56:30 -0500

    template pour débuter liste todo

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,14 @@
+let liste = [
+	{
+		description: 'Maitriser le HTML',
+		fait: true,
+	},
+	{
+		description: 'Hacker la NASA avec le HTML',
+		fait: false,
+	},
+	{
+		description: 'Chanter dans la douche',
+		fait: false,
+	}
+];
