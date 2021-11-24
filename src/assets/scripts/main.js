7c4b11c5aa52950273dc8eba7a2bb95e5c7313de
ReineBeth <simplement.eli@hotmail.com>
2021-11-23 20:19:49 -0500

    Template avec mise en page fait

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,5 +10,19 @@ let liste = [
 	{
 		description: 'Chanter dans la douche',
 		fait: false,
+	},
+	{
+		description: 'Jouer à Hadès',
+		fait: true,
 	}
 ];
+
+const appTodo = {
+	data() {
+	  	return {
+			message : 'Bonjour Vue!'
+		};
+	},
+ };	
+
+Vue.createApp(appTodo).mount('#app_todo');
